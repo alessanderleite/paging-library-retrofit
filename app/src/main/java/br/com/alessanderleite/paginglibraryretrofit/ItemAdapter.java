b0c5a58c4ff3e02c80ce0edfbc298cba05d43074
alessanderleite <alessander.logistica@gmail.com>
2019-06-04 17:16:24 -0300

    create the ItemAdapter class to fetch the data page

---
+++ app/src/main/java/br/com/alessanderleite/paginglibraryretrofit/ItemAdapter.java
@@ -0,0 +1,72 @@
+package br.com.alessanderleite.paginglibraryretrofit;
+
+import android.arch.paging.PagedListAdapter;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.util.DiffUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+public class ItemAdapter extends PagedListAdapter<Item, ItemAdapter.ItemViewHolder> {
+
+    private Context context;
+
+    public ItemAdapter(Context context) {
+        super(DIFF_CALLBACK);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recyclerview_users, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        Item item = getItem(position);
+
+        if (item != null) {
+            holder.textView.setText(item.owner.display_name);
+
+            Glide.with(context)
+                    .load(item.owner.profile_image)
+                    .into(holder.imageView);
+        } else {
+            Toast.makeText(context, "Item is null", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private static DiffUtil.ItemCallback<Item> DIFF_CALLBACK = new DiffUtil.ItemCallback<Item>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull Item oldItem, @NonNull Item newItem) {
+            return oldItem.question_id == newItem.question_id;
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Item oldItem, @NonNull Item newItem) {
+            boolean equals = oldItem.equals(newItem);
+            return equals;
+        }
+    };
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textView;
+        private ImageView imageView;
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textView = (TextView) itemView.findViewById(R.id.textViewName);
+            imageView = (ImageView) itemView.findViewById(R.id.image_View);
+        }
+    }
+}
